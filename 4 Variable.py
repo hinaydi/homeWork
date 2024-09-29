2049cc22d476fed6027847ef9bc3969250499588
hinaydi <hinaydi@gmail.com>
2024-09-30 01:03:05 +1000

    Update 4 Variable.py

--- 4 Variable.py
+++ 4 Variable.py
@@ -1,6 +1,6 @@
-complit_homework = 12
+complited_tasks = 12
 hours_spent = 1.5
 course_name = "Python"
-print (f"Курс: {course_name}, всего задач: {complit_homework},"
+print (f"Курс: {course_name}, всего задач: {complited_tasks},"
        f" затрачено часов: {hours_spent}, среднее время выполнения"
-       f" {hours_spent/complit_homework} часа.")
+       f" {hours_spent/complited_tasks} часа.")
