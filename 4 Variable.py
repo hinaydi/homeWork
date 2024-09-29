a50e660133e0fc9248004da97c4cad2ea842d467
hinaydi <hinaydi@gmail.com>
2024-09-30 00:52:01 +1000

    Add files via upload

---
+++ 4 Variable.py
@@ -0,0 +1,6 @@
+complit_homework = 12
+hours_spent = 1.5
+course_name = "Python"
+print (f"Курс: {course_name}, всего задач: {complit_homework},"
+       f" затрачено часов: {hours_spent}, среднее время выполнения"
+       f" {hours_spent/complit_homework} часа.")
