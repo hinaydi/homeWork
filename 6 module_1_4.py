d95431ff11e95f92dd0a13296181220ab4c0d78e
hinaydi <hinaydi@gmail.com>
2024-10-01 23:04:09 +1000

    Add files via upload

---
+++ 6 module_1_4.py
@@ -0,0 +1,9 @@
+my_string = input('жизнь прекрасна? ')
+print('длина строки' ,len(my_string), 'символов')
+
+my_string = 'жизнь прекрасна?'
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace(' ' ,  ''))
+print(my_string [0])
+print (my_string [-1])
